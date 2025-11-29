20ee1417a33e79b977354609f4d7e3b3862d04f0
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-29 19:27:50 +0530

    feat : add feature to display support text in empty cart

--- Development-Books-Store/src/components/EmptyCart/index.jsx
+++ Development-Books-Store/src/components/EmptyCart/index.jsx
@@ -4,6 +4,7 @@ const EmptyCart = () => {
 	return (
 		<section className="absolute top-50 left-35 opacity-50 sm:left-40 xl:left-1/3 2xl:left-1/3 ">
 			<img src="../public/add-to-cart.png" alt="empty cart" className="w-35" />
+			<h3 className=" font-bold mt-3">Your Cart is Empty...</h3>
 		</section>
 	);
 };
--- Development-Books-Store/src/components/EmptyCart/index.test.jsx
+++ Development-Books-Store/src/components/EmptyCart/index.test.jsx
@@ -6,6 +6,7 @@ function renderComponent() {
 	render(<EmptyCart />);
 	return {
 		emptyCartImg: screen.getByRole("img"),
+		supportText: screen.queryByText(/your cart is empty.../i),
 	};
 }
 
@@ -13,5 +14,9 @@ describe("test cases for empty cart component", () => {
 	it("should render empty cart image", () => {
 		const { emptyCartImg: emptyCartImg } = renderComponent();
 		expect(emptyCartImg).toHaveAttribute("src", "../public/add-to-cart.png");
+	});
+	it("should render support message", () => {
+		const { supportText: supportText } = renderComponent();
+		expect(supportText).toBeInTheDocument();
 	});
 });
